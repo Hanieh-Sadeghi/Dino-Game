12a0aee3888b5bbc03c53f0588f41150cba8401f
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-12 13:56:45 +0330

    add alert

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,30 @@
 let character = document.getElementById("character");
-let tree = document.createElement("tree");
+let tree = document.getElementById("tree");
 let counter = 0;
-
   
 function jump() {
-    if (character.classList == "animate"){return}
+    if (character.classList.contains("animate")) { return; }
     character.classList.add("animate");
-    setInterval(function(){
+    setTimeout(function() {
         character.classList.remove("animate");
     }, 300);
-}       
+}
+       
 
 let checkDead = setInterval( function(){
-    let characterTop = parseInt(window.getComputedStyle(characterTop).getPropertyValue("top"));
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
-    if (treeLeft < 20 && treeLeft > -20 && characterTop >= 130) 
-    {
+    
+    if (treeLeft < 20 && treeLeft > -20 && characterTop >= 130) {
+        tree.style.animation = "none"
+        alert("Game Over. Score : " + Math.floor(counter/100));
+        counter = 0;
+        tree.style.animation = "tree 1s infinite";
+
 
     }else{
         counter++;
-        document.getElementById("s")
+        document.getElementById("scorespan").innerHTML =  Math.floor(counter/100);        
     }
 
 } , 10);

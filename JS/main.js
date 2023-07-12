e95b19ebc898f66542dafe689d8fb37d95a9405a
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-12 18:24:17 +0330

    finish js

--- JS/main.js
+++ JS/main.js
@@ -9,22 +9,22 @@ function jump() {
         character.classList.remove("animate");
     }, 300);
 }
-       
 
-let checkDead = setInterval( function(){
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
-    
-    if (treeLeft < 20 && treeLeft > -20 && characterTop >= 130) {
-        tree.style.animation = "none"
-        alert("Game Over. Score : " + Math.floor(counter/100));
-        counter = 0;
-        tree.style.animation = "tree 1s infinite";
+let checkDead = setInterval(function (){
 
 
-    }else{
-        counter++;
-        document.getElementById("scorespan").innerHTML =  Math.floor(counter/100);        
-    }
+  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+  let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
+  if(treeLeft<20 && treeLeft>-30 && characterTop>=130)
+  {
+    tree.style.animation="none";
+    alert("Game Over. Score : "+Math.floor(counter/100));
+    counter = 0;
+    tree.style.animation = "block 1s infinite linear";
+  }
+  else{
+      counter ++;
+      document.getElementById("scorespan").innerHTML = Math.floor(counter/100);
+  }
+},10);
 
-} , 10);

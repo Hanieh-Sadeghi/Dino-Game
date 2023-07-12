11d868818891a9cdc3f202b5666f5bfe027b125b
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-12 13:10:56 +0330

    add

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,25 @@
-console.log("hanie");
+let character = document.getElementById("character");
+let tree = document.createElement("tree");
+let counter = 0;
+
+  
+function jump() {
+    if (character.classList == "animate"){return}
+    character.classList.add("animate");
+    setInterval(function(){
+        character.classList.remove("animate");
+    }, 300);
+}       
+
+let checkDead = setInterval( function(){
+    let characterTop = parseInt(window.getComputedStyle(characterTop).getPropertyValue("top"));
+    let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
+    if (treeLeft < 20 && treeLeft > -20 && characterTop >= 130) 
+    {
+
+    }else{
+        counter++;
+        document.getElementById("s")
+    }
+
+} , 10);

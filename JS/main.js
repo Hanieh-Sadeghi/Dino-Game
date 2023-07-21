62b91cbb9886d4ba6eb28432ca7c1a762b1066da
Hanieh <105555666+Hanieh-Sadeghi@users.noreply.github.com>
2023-07-21 13:38:37 +0330

    Update main.js
    change and add jump keydown

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,32 @@
 let character = document.getElementById("character");
-let tree = document.getElementById("tree");
-let counter = 0;
-  
-function jump() {
-    if (character.classList.contains("animate")) { return; }
-    character.classList.add("animate");
-    setTimeout(function() {
-        character.classList.remove("animate");
-    }, 300);
-}
+        let tree = document.getElementById("tree");
+        let counter = 0;
 
-let checkDead = setInterval(function (){
+        // افزودن رویداد کلید فشرده‌شده به صفحه
+        document.addEventListener("keydown", function(event) {
+            if (event.keyCode === 32) {
+                jump();
+            }
+        });
 
+        function jump() {
+            if (character.classList.contains("animate")) { return; }
+            character.classList.add("animate");
+            setTimeout(function() {
+                character.classList.remove("animate");
+            }, 300);
+        }
 
-  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
-  if(treeLeft<20 && treeLeft>-30 && characterTop>=130)
-  {
-    tree.style.animation="none";
-    alert("Game Over. Score : "+Math.floor(counter/100));
-    counter = 0;
-    tree.style.animation = "block 1s infinite linear";
-  }
-  else{
-      counter ++;
-      document.getElementById("scorespan").innerHTML = Math.floor(counter/100);
-  }
-},10);
-
+        let checkDead = setInterval(function (){
+            let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+            let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
+            if (treeLeft < 20 && treeLeft > -30 && characterTop >= 130) {
+                tree.style.animation = "none";
+                alert("Game Over. Score : " + Math.floor(counter / 100));
+                counter = 0;
+                tree.style.animation = "block 1s infinite linear";
+            } else {
+                counter++;
+                document.getElementById("scorespan").innerHTML = Math.floor(counter / 100);
+            }
+  }, 10);

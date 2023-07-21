ce235d8baea5c1e5ffb62d38430827747dc38e7b
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-07-21 14:09:44 +0330

    add function createTree

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,56 @@
 let character = document.getElementById("character");
-        let tree = document.getElementById("tree");
-        let counter = 0;
+let tree = document.getElementById("tree");
+let counter = 0;
 
-        // افزودن رویداد کلید فشرده‌شده به صفحه
-        document.addEventListener("keydown", function(event) {
-            if (event.keyCode === 32) {
-                jump();
-            }
-        });
 
-        function jump() {
-            if (character.classList.contains("animate")) { return; }
-            character.classList.add("animate");
-            setTimeout(function() {
-                character.classList.remove("animate");
-            }, 300);
-        }
+function createTree() {
+    const treeTypes = ["images/tree1.png", "tree2.png", "tree3.png"];
+    for (let i = 0; i < 2; i++) {
+        const treeHeight = Math.floor(Math.random() * 50) + 100; // ارتفاع تصادفی بین 100 و 150 پیکسل
+        const treeLeft = Math.floor(Math.random() * 100) + 1; // موقعیت افقی تصادفی بین 1 و 100 درصد
+        const treeType = treeTypes[Math.floor(Math.random() * treeTypes.length)];
 
-        let checkDead = setInterval(function (){
-            let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-            let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
-            if (treeLeft < 20 && treeLeft > -30 && characterTop >= 130) {
-                tree.style.animation = "none";
-                alert("Game Over. Score : " + Math.floor(counter / 100));
-                counter = 0;
-                tree.style.animation = "block 1s infinite linear";
-            } else {
-                counter++;
-                document.getElementById("scorespan").innerHTML = Math.floor(counter / 100);
-            }
-  }, 10);
+        const tree = document.createElement("img");
+        tree.setAttribute("src", treeType);
+        tree.setAttribute("class", "tree");
+        tree.style.height = treeHeight + "px";
+        tree.style.left = treeLeft + "%";
+
+        const treesContainer = document.getElementById("trees-container");
+        treesContainer.appendChild(tree);
+    }
+}
+ // افزودن رویداد کلید فشرده‌شده به صفحه
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 32) {
+        jump();
+    }
+});
+
+function jump() {
+    if (character.classList.contains("animate")) { return; }
+    character.classList.add("animate");
+    setTimeout(function() {
+        character.classList.remove("animate");
+    }, 300);
+}
+
+let checkDead = setInterval(function (){
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let treeLeft = parseInt(window.getComputedStyle(tree).getPropertyValue("left"));
+    if (treeLeft < 20 && treeLeft > -30 && characterTop >= 130) {
+        tree.style.animation = "none";
+        alert("Game Over. Score : " + Math.floor(counter / 100));
+        counter = 0;
+        tree.style.animation = "block 1s infinite linear";
+    } else {
+        counter++;
+        document.getElementById("scorespan").innerHTML = Math.floor(counter / 100);
+    }
+}, 10);
+
+
+        // ایجاد چندتا درخت رندوم
+for (let i = 0; i < 5; i++) {
+    createTree();
+}
